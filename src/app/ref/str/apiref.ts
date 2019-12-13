69939244eee014ece320c6b4fbc0e14dcd7f99d5
Nephera <davidray122780@yahoo.com>
2019-12-12 19:18:58 -0800

    Environment Update
    Updated environment variables to accommodate a staging environment that does not use live DB.

--- src/app/ref/str/apiref.ts
+++ src/app/ref/str/apiref.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class apiref {
   hostname() {
-      /* DEVELOPMENT */
-      //return "http://localhost:3000";
-      /* PRODUCTION */
-      return ""; // https://www.ffrecruiter.com
+    if(environment.production == true)
+      return "";
+    else
+      return "http://localhost:3000";
   }
 }
